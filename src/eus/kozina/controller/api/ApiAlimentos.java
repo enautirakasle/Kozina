2f33325b8c408886b7aedcc1f0402cb119e20072
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-03-08 11:48:49 +0100

    alimentoak itzultzen dituen servleta

---
+++ src/eus/kozina/controller/api/ApiAlimentos.java
@@ -0,0 +1,63 @@
+package eus.kozina.controller.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONStringer;
+
+import eus.kozina.model.bean.Alimento;
+import eus.kozina.model.daoimpl.AlimentoModeloImp;
+
+/**
+ * Servlet implementation class ApiAlimentos
+ */
+@WebServlet("/api/alimentos")
+public class ApiAlimentos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiAlimentos() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		AlimentoModeloImp alimentoModelo =new AlimentoModeloImp();
+		ArrayList<Alimento> alimentos = alimentoModelo.selectAll();
+		
+		String jsonString = JSONStringer.valueToString(alimentos);
+		
+		PrintWriter out = response.getWriter();
+		
+		response.setHeader("Access-Control-Allow-Origin","*"); //jsonp deia denean ez da behar
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		
+		out.print(jsonString);
+		out.flush();
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
