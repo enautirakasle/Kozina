dc1b8407edbf16e5e5ff2616788d08bd19b7c2cc
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-03-27 13:51:44 +0100

    commentar doGet en GuardarAlimento.java

--- src/eus/kozina/controller/alimento/GuardarAlimento.java
+++ src/eus/kozina/controller/alimento/GuardarAlimento.java
@@ -28,11 +28,12 @@ public class GuardarAlimento extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+	 */
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
