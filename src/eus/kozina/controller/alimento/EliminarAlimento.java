515f4c653ec961008d3f0969d0fd20d9f29c16c2
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-03-27 13:52:25 +0100

    commentar doPost en EliminarAlimento.java

--- src/eus/kozina/controller/alimento/EliminarAlimento.java
+++ src/eus/kozina/controller/alimento/EliminarAlimento.java
@@ -49,10 +49,11 @@ public class EliminarAlimento extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	*/
 
 }
