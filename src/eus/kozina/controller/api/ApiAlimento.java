00ca93980ac2d1c915dd05f3f9fd879461d623e7
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-05-17 11:06:48 +0200

    primer commit

---
+++ src/eus/kozina/controller/api/ApiAlimento.java
@@ -0,0 +1,62 @@
+package eus.kozina.controller.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import eus.kozina.model.bean.Alimento;
+import eus.kozina.model.daoimpl.AlimentoModeloImp;
+
+/**
+ * Servlet implementation class ApiAlimento
+ */
+@WebServlet("/api/alimento")
+public class ApiAlimento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiAlimento() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idAlimento = Integer.parseInt(request.getParameter("idAlimento"));
+		
+		AlimentoModeloImp alimentoModelo =new AlimentoModeloImp();
+		Alimento alimento = alimentoModelo.select(idAlimento);
+		
+		JSONObject jsonAlimento = new JSONObject(alimento);
+		PrintWriter out = response.getWriter();
+		
+		response.setHeader("Access-Control-Allow-Origin","*"); //jsonp deia denean ez da behar
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		
+		out.print(jsonAlimento);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
