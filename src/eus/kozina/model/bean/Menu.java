d48f8c5e733add10b32510c199802ba608315cde
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-06-20 13:43:31 +0200

    anadir getSetters y algo mas en bean.Menu.java

--- src/eus/kozina/model/bean/Menu.java
+++ src/eus/kozina/model/bean/Menu.java
@@ -3,8 +3,47 @@ package eus.kozina.model.bean;
 import java.util.ArrayList;
 
 public class Menu {
+	
 	private int id;
 	private ArrayList<Plato> primeros;
 	private ArrayList<Plato> segundos;
 	private ArrayList<Plato> postres;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Plato> getPrimeros() {
+		return primeros;
+	}
+	public void setPrimeros(ArrayList<Plato> primeros) {
+		this.primeros = primeros;
+	}
+	public ArrayList<Plato> getSegundos() {
+		return segundos;
+	}
+	public void setSegundos(ArrayList<Plato> segundos) {
+		this.segundos = segundos;
+	}
+	public ArrayList<Plato> getPostres() {
+		return postres;
+	}
+	public void setPostres(ArrayList<Plato> postres) {
+		this.postres = postres;
+	}
+	
+	public void addPrimerPlato(Plato plato) {
+		this.primeros.add(plato);
+	}
+	
+	public void addSegundoPlato(Plato plato) {
+		this.segundos.add(plato);
+	}
+	
+	public void addPostre(Plato plato) {
+		this.postres.add(plato);
+	}
+	
 }
