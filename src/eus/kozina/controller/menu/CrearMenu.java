4de7ffed92dd2da60fb9745584445ad7ae903330
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-11-11 23:21:10 +0100

    aldaketa txikia

--- src/eus/kozina/controller/menu/CrearMenu.java
+++ src/eus/kozina/controller/menu/CrearMenu.java
@@ -28,7 +28,7 @@ public class CrearMenu extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PlatoModelo platoModelo = new PlatosModeloImp();
+		//PlatoModelo platoModelo = new PlatosModeloImp();
 		
 	}
 
